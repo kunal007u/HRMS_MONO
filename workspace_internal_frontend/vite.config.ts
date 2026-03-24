5b0f1e4491ee1dce1081750c32c6969720109db5
Krunal <kunalrkathiriya123@gmail.com>
2026-03-23 20:14:18 -0500

    Update Vite configuration for clarity and formatting

--- workspace_internal_frontend/vite.config.ts
+++ workspace_internal_frontend/vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // This forces absolute paths for your assets
   server: {
     host: '0.0.0.0',
-    port: 5003, // specify your desired port here
+    port: 5003,
   },
 })
